50b5610a5147c325d3b130ff99021dfe5f6c6c6c
idwxy <idwxy@live.com>
2019-11-19 18:36:42 +0800

    日志打印

---
+++ src/main/java/com/idwxy/debug/LogController.java
@@ -0,0 +1,24 @@
+package com.idwxy.debug;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class LogController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+
+    @GetMapping("/log")
+    @ResponseBody
+    public String log() {
+        logger.info("这是一条 info 级别的日志");
+        logger.error("这是一条 error 级别的日志");
+        logger.debug("这是一条 debug 级别的日志");
+        logger.warn("这是一条 warn 级别的日志");
+        return "日志打印";
+    }
+}
