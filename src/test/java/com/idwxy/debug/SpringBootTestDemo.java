3a0610ada2282e4d71aa0c66cdb2da05c12715c6
idwxy <idwxy@live.com>
2019-11-20 18:31:15 +0800

    简单的单元测试

---
+++ src/test/java/com/idwxy/debug/SpringBootTestDemo.java
@@ -0,0 +1,12 @@
+package com.idwxy.debug;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SpringBootTestDemo {
+
+    @Test
+    public void test1() {
+        System.out.println("test1");
+    }
+}
