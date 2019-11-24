bf232bc0461255676f8fb6c74acdc62f33acf2cb
idwxy <idwxy@live.com>
2019-11-24 16:38:03 +0800

    POST test

--- src/main/java/com/idwxy/debug/controller/TestController.java
+++ src/main/java/com/idwxy/debug/controller/TestController.java
@@ -2,6 +2,7 @@ package com.idwxy.debug.controller;
 
 import com.idwxy.debug.entity.SaleGoods;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,5 +20,14 @@ public class TestController {
         saleGoods.setType(1);
         saleGoods.setWeight(300);
         return saleGoods;
+    }
+
+    @RequestMapping(value = "/test/httpmessageconverter", method = RequestMethod.POST)
+    @ResponseBody
+    public SaleGoods httpMessageConverterTest2(@RequestBody SaleGoods saleGoods) {
+        System.out.println(saleGoods.toString());
+        saleGoods.setWeight(saleGoods.getType() + 1);
+        saleGoods.setGoodsName("商品名：" + saleGoods.getGoodsName());
+        return saleGoods
     }
 }
