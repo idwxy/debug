6b76c1c7a672c1b75a418532a993a53c2c55df9d
idwxy <idwxy@live.com>
2019-11-30 18:04:25 +0800

    演示

--- src/main/java/com/idwxy/debug/controller/ThymeleafController.java
+++ src/main/java/com/idwxy/debug/controller/ThymeleafController.java
@@ -1,6 +1,7 @@
 package com.idwxy.debug.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -14,5 +15,19 @@ public class ThymeleafController {
     "springboot-thymeleaf") String description) {
         request.setAttribute("description", description);
         return "thymeleaf";
+    }
+
+    @GetMapping("/attributes")
+    public String attributes(ModelMap map) {
+        // 更改 h1 内容
+        map.put("title", "Thymeleaf 标签演示");
+        // 更改 id、name、value
+        map.put("th_id", "thymeleaf-input");
+        map.put("th_name", "thymeleaf-input");
+        map.put("th_value", "13");
+        // 更改 class、href
+        map.put("th_class", "thymeleaf-class");
+        map.put("th_href", "http://13blog.site");
+        return "attributes";
     }
 }
