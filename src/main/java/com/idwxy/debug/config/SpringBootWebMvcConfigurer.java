3c09003b4ac4b2aed289d58b6af2ab1defdac2f3
idwxy <idwxy@live.com>
2019-12-09 17:05:58 +0800

    upload

---
+++ src/main/java/com/idwxy/debug/config/SpringBootWebMvcConfigurer.java
@@ -0,0 +1,12 @@
+package com.idwxy.debug.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringBootWebMvcConfigurer implements WebMvcConfigurer {
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**").addResourceLocations(("file:D:/upload/"));
+    }
+}
